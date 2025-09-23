e65d4ffc8854d68272a2fbabeda1888c2327d22c
Giuliann de Sousa Duarte <sgiuliann@gmail.com>
2025-09-23 11:51:32 -0300

    Função: Novo Usuario
    Foi implementado a função de adicionar novos usuários ao banco de dados.

--- python/main.py
+++ python/main.py
@@ -1,15 +1,19 @@
 def menu():
 	# Imports dos arquivos:
 	from vizualizar import vizu
+	from new_user import new_user
 
 	print('Bem-Vindo ao Menu do Banco de Dados\n\n')
 	print('(1) Vizualiza o banco de dados')
-	print('(2) Sair')
+	print('(2) Adicionar um novo usuario ao banco')
+	print('(3) Sair')
 	
 	select = int(input())
 	match select:
 		case 1:
 			vizu()
 		case 2:
+			new_user()
+		case 3:
 			exit()
 menu()
