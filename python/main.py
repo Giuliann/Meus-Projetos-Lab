638946f89bf90f4da80fac4007835544bfbdffb9
Giuliann de Sousa Duarte <sgiuliann@gmail.com>
2025-09-23 11:56:05 -0300

    Função: Deletar Usuários
    Foi incrementado a função de deletar/remover usuários do banco de dados

--- python/main.py
+++ python/main.py
@@ -2,11 +2,13 @@ def menu():
 	# Imports dos arquivos:
 	from vizualizar import vizu
 	from new_user import new_user
+	from delete_user import delete
 
 	print('Bem-Vindo ao Menu do Banco de Dados\n\n')
 	print('(1) Vizualiza o banco de dados')
 	print('(2) Adicionar um novo usuario ao banco')
-	print('(3) Sair')
+	print('(3) Remove um usuario do banco')
+	print('(4) Sair')
 	
 	select = int(input())
 	match select:
@@ -15,5 +17,7 @@ def menu():
 		case 2:
 			new_user()
 		case 3:
+			delete()
+		case 4:
 			exit()
 menu()
