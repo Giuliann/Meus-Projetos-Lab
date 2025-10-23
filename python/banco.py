ba9525e4c46786432334e4182d99008931003ca3
Giuliann de Sousa Duarte <sgiuliann@gmail.com>
2025-10-23 13:56:01 -0300

    fazendo leitura de um banco de dados

--- python/banco.py
+++ python/banco.py
@@ -2,5 +2,8 @@ def db():
     from sqlalchemy import create_engine
 
     # Banco de dados local:
-    db = create_engine("sqlite:///banco_local/meu_banco.db")
+
+    db = create_engine("sqlite:///banco_local/Dos_Csv.db", echo=False)
+
+
     return(db)
--- python/vizualizar.py
+++ python/vizualizar.py
@@ -3,24 +3,46 @@ def vizu():
     import numpy as np
     from sqlalchemy import inspect, text
 
-    # Exibe os nomes das tabelas existentes do banco.
-    insp = inspect(db())
-    tabelas = insp.get_table_names()
-    print('Tabelas no banco:', tabelas)
+    # Acessando o banco: 
+    banco = db()
+    insp = inspect(banco)
 
-    # Faz conexão com o banco local, pegas as linhas da tabela e mostra elas.
-    with db().connect() as conexao:
-        resultado = conexao.execute(text("SELECT * FROM usuarios"))
+    # Pegando os nomes das tabelas: 
+    nomes_tabelas = insp.get_table_names()
+    print('Tabelas no banco: ')
+    for nomeT in nomes_tabelas:
+        print(f" - {nomeT}")
+
+    # Fazendo Acesso das Colunas e Linhas da Tabela:  
+    with banco.connect() as conexao:
+        resultado = conexao.execute(text("SELECT * FROM tabela_csv"))
         linhas = resultado.fetchall()
+        colunas = resultado.keys()
 
-    # pra evitar poluição.
-    print('Primeiras linhas:', linhas[:5])
+    # Lista com os nomes das colunas da Tabela: 
+    print('Colunas Listadas dentro da Tabela do Banco: ')
+    for nomeC in colunas:
+        print(f" - {nomeC}")
 
-    # conversão pra numpy.
+    print("\n\nPrimeiras linhas:\n",linhas[:1])
+
+    # Pra as informaçôes não sumirem do terminal: 
+    input("\nPressione ENTER para ver as ocorrências...")
+
+    # Transformando em Numpy
+    data = np.array(linhas)
+    print(f"\nNúmero de linhas e colunas: {data.shape}\n")
+
+    # Exibe as arrays
+    for data_array in data:
+        print(f"\n{data_array}")
+
     coluna = np.array([linha[0] for linha in linhas])
     print('Array NumPy:', coluna)
 
-    # Contar a frequência de cada valor em uma coluna.
+    # Exibe os valores da lista e a contagem de ocorrencias: 
     valores, contagens = np.unique(coluna, return_counts=True)
-    for v, c in zip(valores, contagens):
-        print(f"{v} → {c} ocorrências")
+    for val, oc in zip(valores, contagens):
+        print(f"{val} → {oc} ocorrências")
+
+    
