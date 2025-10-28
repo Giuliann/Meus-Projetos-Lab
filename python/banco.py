b162020873f37420e2fa5825a40f3223bab68d38
Giuliann de Sousa Duarte <sgiuliann@gmail.com>
2025-10-27 22:18:23 -0300

    Atualização de Unificação
    Criação de uma Classe para unificar as operações  de acesso e visualização dos bancos de dados, remoção de código duplicado do arquivo de visualização

--- python/banco.py
+++ python/banco.py
@@ -1,9 +1,49 @@
-def db():
-    from sqlalchemy import create_engine
-
-    # Banco de dados local:
-
-    db = create_engine("sqlite:///banco_local/Dos_Csv.db", echo=False)
+from sqlalchemy import create_engine
+import numpy as np
+from sqlalchemy import inspect, text
 
 
-    return(db)
+class Banco():
+        def __init__(self, nome):
+            self.nome = nome
+            self.local = create_engine("sqlite:///banco_local/{}.db".format(self.nome), echo=False)
+            self.insp = inspect(self.local)
+
+        # Pegando os nomes das tabelas: 
+        def nome_tabela(self):
+            nomes_tabelas = self.insp.get_table_names()
+            print('Tabelas no banco: ')
+            for nomeT in nomes_tabelas:
+                print(f" - {nomeT}")
+        
+        # Fazendo Acesso das Colunas e Linhas da Tabela: 
+        def conexao(self):
+            with self.local.connect() as conexao:
+                resultado = conexao.execute(text("SELECT * FROM tabela_csv"))
+            self.linhas = resultado.fetchall()
+            self.colunas = resultado.keys()
+
+        # Exibindo nome das colunas: 
+        def nome_coluna(self):
+            print('Colunas Listadas dentro da Tabela do Banco: ')
+            for nomeC in self.colunas:
+                print(f" - {nomeC}")
+
+        # Transformando em Numpy array e exibindo o número de linhas e colunas do banco: 
+        def array_numpy(self):
+            self.data = np.array(self.linhas)
+            print(f"\nNúmero de linhas e colunas: {self.data.shape}\n")
+
+
+        # Exibe as arrays
+        def exibir(self):
+            for data_array in self.data:
+                print(f"\n{data_array}")
+
+        # Exibe os valores da primeira coluna e a contagem de ocorrencias:
+        def ocorrencias(self):
+            coluna = np.array([linha[0] for linha in self.linhas])
+            valores, contagens = np.unique(coluna, return_counts=True)
+            for val, oc in zip(valores, contagens):
+                print(f"{val} → {oc} ocorrências")
+
--- python/main.py
+++ python/main.py
@@ -1,15 +1,86 @@
 def menu():
-	# Imports dos arquivos:
-	from vizualizar import vizu
+	# Importando a Classe: 
+	from banco import Banco
 
-	print('Bem-Vindo ao Menu do Banco de Dados\n\n')
-	print('(1) Consultar o banco de dados "Dos_Csv"')
-	print('(2) Sair')
-	
-	select = int(input())
+	# Chamando classe e nomeando os objetos: 
+	Dos_Csv = Banco(nome='Dos_Csv')
+	Intrusion_Csv = Banco(nome='Intrusion_Csv')
+	MitM_Csv = Banco(nome='MitM_Csv')
+
+	# Menu
+	print('\n\nBem-Vindo ao Menu do Banco de Dados\n\n')
+	print('(1) Acessar banco de dados Dos_Csv')
+	print('(2) Aecssar banco de dados Instrsion_Csv')
+	print('(3) Acessar banco de dados Mitm_Csv')
+	print('(4) Encerrar programa')
+
+	# Seleção das opções do menu: 
+	select = int(input())	
 	match select:
 		case 1:
-			vizu()
+			# Dados do Banco Dos_Csv.db: 
+			Dos_Csv.nome_tabela()
+			Dos_Csv.conexao()
+			Dos_Csv.nome_coluna()
+			Dos_Csv.array_numpy()
+			input('Pressione Enter para continuar com a vizualização das Arrays...')
+			Dos_Csv.exibir()
+			input('Pressione Enter para continuar com a vizualização das Ocorrencias...')
+			Dos_Csv.ocorrencias()
+			print('\n\nDeseja voltar para o menu?')
+
+			# Voltar para o menu: 
+			print('(1) Sim')
+			print('(2) Não')
+			select = int(input(''))
+			match select:
+				case 1:
+					menu()
+				case 2:
+					exit('Promgrama encerrado...')
 		case 2:
-			exit(print('Programa Fechado...'))
+			# Dados do Banco Instrusio_Csv.db: 
+			Intrusion_Csv.nome_tabela()
+			Intrusion_Csv.conexao()
+			Intrusion_Csv.nome_coluna()
+			Intrusion_Csv.array_numpy()
+			input('Pressione Enter para continuar com a vizualização das Arrays...')
+			Intrusion_Csv.exibir()
+			input('Pressione Enter para continuar com a vizualização das Ocorrencias...')
+			Intrusion_Csv.ocorrencias()
+			print('\n\nDeseja voltar para o menu?')
+
+			# Voltar para o menu: 
+			print('(1) Sim')
+			print('(2) Não')
+			select = int(input(''))
+			match select:
+				case 1:
+					menu()
+				case 2:
+					exit('Promgrama encerrado...')
+		case 3:
+			# Dados do Banco MitM_Csv.db: 
+			MitM_Csv.nome_tabela()
+			MitM_Csv.conexao()
+			MitM_Csv.nome_coluna()
+			MitM_Csv.array_numpy()
+			input('Pressione Enter para continuar com a vizualização das Arrays...')
+			MitM_Csv.exibir()
+			input('Pressione Enter para continuar com a vizualização das Ocorrencias...')
+			MitM_Csv.ocorrencias()
+			print('\n\nDeseja voltar para o menu?')
+
+			# Voltar para o menu: 
+			print('(1) Sim')
+			print('(2) Não')
+			select = int(input(''))
+			match select:
+				case 1:
+					menu()
+				case 2:
+					exit('Promgrama encerrado...')
+		case 4:
+			exit('Programa encerrado...') 
+	
 menu()
